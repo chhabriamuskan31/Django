d7460ac52b64cba4014b776a0069dafa6912958f
chhabriamuskan31 <muskanchhabria2002@gmail.com>
2022-09-11 15:11:12 +0530

    Created basic django website

--- blog-container/blog/blogapp/views.py
+++ blog-container/blog/blogapp/views.py
@@ -1,3 +1,4 @@
+from turtle import title
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.db import connection 
@@ -27,5 +28,40 @@ def insert(request):
     content = request.POST['content']
     cursor = connection.cursor()
     cursor.execute("INSERT INTO posts (`title`,`content`) VALUES ( %s, %s );", (title, content))
+    return redirect('/blog/home')
+
+
+def edit(request, pk):
+    print(pk)
+    cursor = connection.cursor()
+    cursor.execute(f'SELECT * from posts where softdelete = 0 and id = {pk}')
+
+    # A list of dictionary
+    columns = [col[0] for col in cursor.description]
+    posts =  [
+        dict(zip(columns, row))
+        for row in cursor.fetchall()
+    ]
+    context = {
+        'keyposts': posts[0]
+    }
+    print(context)
+    return render(request,'blogapp/editform.html',context)
+
+
+def update(request):
+    title= request.POST['blogTitle']
+    content = request.POST['content']
+    id = request.POST['id']
+    cursor = connection.cursor()
+    # print(title, content, id)
+    cursor.execute("UPDATE posts set title = %s ,content= %s where id = %s", (title, content, id))
+    cursor = connection.cursor()
+    return redirect('/blog/home')
+
+def delete(request,pk):
+    cursor = connection.cursor()
+    # print(title, content, id)
+    cursor.execute(f'UPDATE posts set softdelete = 1 where id={pk}')
     return redirect('/blog/home')
 
