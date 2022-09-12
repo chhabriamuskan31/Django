71564d9ba67a1c24cd596f1cd2731b38d2b608fd
chhabriamuskan31 <muskanchhabria2002@gmail.com>
2022-09-12 20:34:32 +0530

    Modified blog app

--- blog-container/blog/blogapp/views.py
+++ blog-container/blog/blogapp/views.py
@@ -1,22 +1,31 @@
+from email import contentmanager
 from turtle import title
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.db import connection 
+from django.db import connection
+from .models import Blog  
 
 # Create your views here.
 def home(request):
     # return HttpResponse("<h1>Hello world</h1>")
     # return render(request,'blogapp/home.html');
-    cursor = connection.cursor()
-    cursor.execute('SELECT * from posts where softdelete = 0')
+    # cursor = connection.cursor()
+    # cursor.execute('SELECT * from posts where softdelete = 0')
 
-    columns = [col[0] for col in cursor.description]
-    posts =  [
-        dict(zip(columns, row))
-        for row in cursor.fetchall()
-    ]
-    context = {
-        'keyposts': posts
+    # columns = [col[0] for col in cursor.description]
+    # posts =  [
+    #     dict(zip(columns, row))
+    #     for row in cursor.fetchall()
+    # ]
+    # context = {
+    #     'keyposts': posts
+    # }
+    # return render(request,'blogapp/home.html',context)
+
+    posts = Blog.objects.filter(softDelete=0)
+    print(posts)
+    context={
+        'keyposts':posts
     }
     return render(request,'blogapp/home.html',context)
 
@@ -26,42 +35,52 @@ def create(request):
 def insert(request):
     title = request.POST['blogTitle']
     content = request.POST['content']
-    cursor = connection.cursor()
-    cursor.execute("INSERT INTO posts (`title`,`content`) VALUES ( %s, %s );", (title, content))
+    # cursor = connection.cursor()
+    # cursor.execute("INSERT INTO posts (`title`,`content`) VALUES ( %s, %s );", (title, content))
+    blog = Blog(title=title, content=content)
+    blog.save()
     return redirect('/blog/home')
-
+    
 
 def edit(request, pk):
     print(pk)
-    cursor = connection.cursor()
-    cursor.execute(f'SELECT * from posts where softdelete = 0 and id = {pk}')
+    # cursor = connection.cursor()
+    # cursor.execute(f'SELECT * from posts where softdelete = 0 and id = {pk}')
 
-    # A list of dictionary
-    columns = [col[0] for col in cursor.description]
-    posts =  [
-        dict(zip(columns, row))
-        for row in cursor.fetchall()
-    ]
+    # # A list of dictionary
+    # columns = [col[0] for col in cursor.description]
+    # posts =  [
+    #     dict(zip(columns, row))
+    #     for row in cursor.fetchall()
+    # ]
+    posts=Blog.objects.get(id=pk)
     context = {
-        'keyposts': posts[0]
+        'keyposts': posts
     }
     print(context)
     return render(request,'blogapp/editform.html',context)
 
 
 def update(request):
+    id = request.POST['id']
     title= request.POST['blogTitle']
     content = request.POST['content']
-    id = request.POST['id']
-    cursor = connection.cursor()
-    # print(title, content, id)
-    cursor.execute("UPDATE posts set title = %s ,content= %s where id = %s", (title, content, id))
-    cursor = connection.cursor()
+    # cursor = connection.cursor()
+    # # print(title, content, id)
+    # cursor.execute("UPDATE posts set title = %s ,content= %s where id = %s", (title, content, id))
+    # cursor = connection.cursor()
+    blog=Blog.objects.get(id=id)
+    blog.title=title
+    blog.content=content
+    blog.save()
+
     return redirect('/blog/home')
 
 def delete(request,pk):
-    cursor = connection.cursor()
-    # print(title, content, id)
-    cursor.execute(f'UPDATE posts set softdelete = 1 where id={pk}')
+    # cursor = connection.cursor()
+    # # print(title, content, id)
+    # cursor.execute(f'UPDATE posts set softdelete = 1 where id={pk}')
+    blog = Blog.objects.get(id=pk)
+    blog.softDelete = 1
+    blog.save()
     return redirect('/blog/home')
-
